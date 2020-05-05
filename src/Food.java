dda5269e1d9b2cb5ab2000d4e4559832c4eb50c4
kabilza <naruoluvohina@gmail.com>
2020-05-05 11:30:33 +0700

    Initial Code

---
+++ src/Food.java
@@ -0,0 +1,7 @@
+public abstract class Food {
+    String description = "Unknown Food";
+    public String getDescription () {
+        return description;
+    }
+    public abstract double cost();
+}
---
+++ src/FoodToppingDecorator.java
@@ -0,0 +1,3 @@
+public abstract class FoodToppingDecorator extends Food {
+    public abstract String getDescription ();
+}
